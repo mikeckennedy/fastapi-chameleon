9deffab80f554ada8dfcb002c34781aa8671801d
Michael Kennedy <mikeckennedy@gmail.com>
2021-10-12 13:38:41 -0700

    Add response() to the all list of exports.

--- fastapi_chameleon/__init__.py
+++ fastapi_chameleon/__init__.py
@@ -2,7 +2,7 @@
 
 __version__ = '0.1.12'
 __author__ = 'Michael Kennedy <michael@talkpython.fm>'
-__all__ = ['template', 'global_init', 'not_found', ]
+__all__ = ['template', 'global_init', 'not_found', 'response', ]
 
 from .engine import global_init
 from .engine import template
